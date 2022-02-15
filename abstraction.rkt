6f82c0de9c8ff83aaf228649c74ae40a90af0405
Andrew Gross <80224229+andrew0144@users.noreply.github.com>
2022-02-15 18:12:49 -0500

    started 5 statements and helper functions

--- abstraction.rkt
+++ abstraction.rkt
@@ -1,18 +1,28 @@
 #lang racket
 ;changed this to prefix
 
+
+;evaluate an expression
+;(define Mexpression
+;  (lambda (expression state)
+ ;   (cond
+  ;   ((
+    
+
+
 ; evaluate the value of an expression
 (define Mvalue
-  (lambda (expression)
+  (lambda (expression state)
     (cond
       ((number? expression) expression)
-      ((eq? (operator expression) '+) (+ (Mvalue (leftoperand expression)) (Mvalue (rightoperand expression))))
-      ((eq? (operator expression) '-) (- (Mvalue (leftoperand expression)) (Mvalue (rightoperand expression))))
-      ((eq? (operator expression) '*) (* (Mvalue (leftoperand expression)) (Mvalue (rightoperand expression))))
-      ((eq? (operator expression) '/) (quotient (Mvalue (leftoperand expression)) (Mvalue (rightoperand expression))))
-      ((eq? (operator expression) '%) (remainder (Mvalue (leftoperand expression)) (Mvalue (rightoperand expression))))
-      (else (error 'badop "Bad operator")))))
-
+      ((exists? expression state) (find expression state))
+      ((eq? (operator expression) '+) (+ (Mvalue (leftoperand expression) state) (Mvalue (rightoperand expression) state)))
+      ((eq? (operator expression) '-) (- (Mvalue (leftoperand expression) state) (Mvalue (rightoperand expression) state)))
+      ((eq? (operator expression) '*) (* (Mvalue (leftoperand expression) state) (Mvalue (rightoperand expression) state)))
+      ((eq? (operator expression) '/) (quotient (Mvalue (leftoperand expression) state) (Mvalue (rightoperand expression) state)))
+      ((eq? (operator expression) '%) (remainder (Mvalue (leftoperand expression) state) (Mvalue (rightoperand expression) state)))
+      (else (Mboolean expression))))) ; might work
+ 
 ; evaluate the truth of a statement
 (define Mboolean
   (lambda (expression)
@@ -29,10 +39,36 @@
       ((eq? (operator expression) '!) (not (Mboolean (leftoperand expression))))
       (else (error 'badop "Bad operator")))))
 
+(define Mdeclare
+  (lambda (expression state)
+    (cond
+      ((not (null? (cdr (cdr expression)))) (cons (list (cadr expression) (Mvalue(caddr expression))) state))
+      ((not (null? (cadr expression))) (cons (list (cadr expression)) state)))))
+
+;assuming exists and insert work properly
+(define Massign
+  (lambda (expression state)
+    (cond
+      ((if (exists? (var state))) (insert (car expression) (cdr expression) state))
+      (else (error 'varnotdeclared "Var not declared")))))
+
+; assuming we pass in expression after "return"
+(define Mreturn
+  (lambda (expression state)
+    (cond
+      (
+      ((if (exists? (var state))) (find var state))
+      (else (error 'invalidreturn "Invalid return"))
+
+; implement exists, check if atom and check if present in state
+
+; implement insert (basically what we did in class)
+
 (define Mstate
   (lambda (expression state)
     (cond
-      ((eq? (operator expression) 'var) (append state (list (cadr expression)) )))))
+      ((eq? (car expression) 'var) (Mdeclare expression state)))))
+      
       
 
 ; helper functions to abstract the operator and operands of binary expressions.
